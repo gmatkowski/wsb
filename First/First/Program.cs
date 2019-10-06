0f3262f22b112c1262450b9f30606701e8fff6ca
student <info@gmatkowski.pl>
2019-10-06 09:21:49 +0200

    if else

--- First/First/Program.cs
+++ First/First/Program.cs
@@ -6,31 +6,62 @@ namespace First
     {
         static void Main(string[] args)
         {
-            Console.Write("Imię: "); //Your name
-            Console.WriteLine("Janusz");
+           /*
+            * użytkownik podaje z klawiatury
+            * długość boku
+           */
             /*
-             * Komentarz
-             * w 
-             * wielu
-             * liniach
+            Console.WriteLine("Podaj bok a:");
+            string a = Console.ReadLine();
+
+            //Console.WriteLine("Bok a wynosi {0}", a;
+
+            double bok_a = double.Parse(a);
+            bok_a *= bok_a;
+
+            Console.WriteLine("Pole kwadratu wynosi {0}", bok_a);
+            Console.ReadKey();
+            */
+            /*
+            Console.WriteLine("Podaj podstawa: ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Podaj wysokość: ");
+            string h = Console.ReadLine();
+
+
+            double bok_a = double.Parse(a);
+            double wysokosc_h = double.Parse(h);
+
+            double result = bok_a * wysokosc_h / 2;
+
+            Console.WriteLine("Pole trójkąta wynosi {0}", result);
             */
 
-            string name = "Anna";
-            Console.WriteLine(name);
-            // + konkatenacja
-            Console.WriteLine("Masz na imię: " + name);
-            Console.Write("Masz na imię: ");
-            Console.WriteLine(name);
+            string str;
+            int liczba;
 
-            Console.WriteLine("Masz na imię: {0}", name);
+            Console.WriteLine("Podaj liczbę całkowitą");
+            str = Console.ReadLine();
 
-            int age = 18;
-            sbyte age1 = 20;
+            //liczba = int.Parse(str);
+            if (int.TryParse(str, out liczba) == false)
+            {
+                Console.WriteLine("Zły format liczby.");
 
-            Console.WriteLine("Imię: {0}, Wiek: {1}", name, age);
-            Console.WriteLine("Imię: {0}, Wiek: {1}", name, age1);
-
-
+            }
+            else
+            {
+                Console.WriteLine("Liczba wynosi {0}", liczba);
+                // Sprawdź czy liczba całkowita podana z klawiatury jest parzysta;
+                if (liczba % 2 == 0)
+                {
+                    Console.WriteLine("Liczba {0}, jest parzysta", liczba);
+                }
+                else {
+                    Console.WriteLine("Liczba {0} nie jest przysta", liczba);
+                }
+            }
 
             Console.ReadKey();
         }
---
+++ First/First/Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Imię: "); //Your name
+            Console.WriteLine("Janusz");
+            /*
+             * Komentarz
+             * w 
+             * wielu
+             * liniach
+            */
+
+            string name = "Anna";
+            Console.WriteLine(name);
+            // + konkatenacja
+            Console.WriteLine("Masz na imię: " + name);
+            Console.Write("Masz na imię: ");
+            Console.WriteLine(name);
+
+            Console.WriteLine("Masz na imię: {0}", name);
+
+            int age = 18;
+            sbyte age1 = 20;
+
+            Console.WriteLine("Imię: {0}, Wiek: {1}", name, age);
+            Console.WriteLine("Imię: {0}, Wiek: {1}", name, age1);
+
+            Console.ReadKey();
+        }
+    }
+}
