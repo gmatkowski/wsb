8b1fa8bea421d4af10f76bee2d9ff7771205549f
gmatkowski <info@gmatkowski>
2019-10-18 15:27:44 +0200

    zadanie pole prostokata

---
+++ Prostokat/Prostokat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prostokat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side_a;
+            int side_b;
+
+            Console.WriteLine("Podaj bok A (cm):");
+            string user_input_side_a = Console.ReadLine();
+            if (int.TryParse(user_input_side_a, out side_a) == false)
+            {
+                Console.WriteLine("Błędne dane podane z klawiatury");
+                return;
+            }
+
+            Console.WriteLine("Podaj bok B (cm):");
+            string user_input_side_b = Console.ReadLine();
+            if (int.TryParse(user_input_side_b, out side_b) == false)
+            {
+                Console.WriteLine("Błędne dane podane z klawiatury");
+                return;
+            }
+
+            int field = side_a * side_b;
+
+            Console.WriteLine("Pole prostokąta wynosi: {0}cm/kw", field);
+            Console.ReadKey();
+        }
+    }
+}
